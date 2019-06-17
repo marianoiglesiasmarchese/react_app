ea2e3915e51a5451768b573d2077fb29233a656e
marianoiglesiasmarchese <marianoiglesiasmarchese@gmail.com>
2019-06-17 16:33:30 -0300

    video(16): updating our order state

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,15 @@ class App extends React.Component{
         });
     };
 
+    addToOrder = (key) => {
+        // 1. take a copy of state
+        const order = {...this.state.order};
+        // 2. either add the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+        // 3. call setStatus to update our state object
+        this.setState({ order });
+    };
+
     render(){
         return (
             <div className="catch-of-the-day">
@@ -43,7 +52,7 @@ class App extends React.Component{
                     <Header tagline="fresh seafood market" bol={true} count="400" /> 
                     <ul className="fishes">
                         {
-                            Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+                            Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
                         }
                     </ul>
                 </div>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component{
+
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    };
+
     render() {
         // ES6 allow us to handle substructures with only needed information for our component
-        const { image, name, price, desc } = this.props.details;
+        const { name, price, desc, status } = this.props.details;
+        const isAvailable = status === 'available';
         return (
             <li className="menu-fish">
-                <image src={image} alt={name}/>
+                {/* <image src={image} alt={name}/> image folder is not in the project */}
                 <h3 className="fish-name">
                     {name}
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button> add to cart! </button>
+                <button disabled={!isAvailable} onClick={this.handleClick}> {isAvailable ? 'add to order!' : 'sold out!'} </button>
             </li>            
         )
     }
